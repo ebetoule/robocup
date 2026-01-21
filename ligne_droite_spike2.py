0af0391beae59cb0a46af3de50acbd356fc4ebb0
Eloïse <eloise.betoule@free.fr>
2026-01-21 17:58:14 +0100

    Code propre, essai de la vitesse

--- ligne_droite_spike2.py
+++ ligne_droite_spike2.py
@@ -4,6 +4,7 @@ import time
 from buildhat import Motor
 from picamera2 import Picamera2
 from pprint import *
+
 #size = (1280,720)
 size = (640,480)
 #size = (480, 270)
@@ -12,9 +13,9 @@ irow = -5
 motor_right = Motor('B')
 motor_left = Motor('A')
 trajectoire = np.zeros((100))
+
 def init_pycam():
     picam2 = Picamera2()
-    #qpprint(picam2.sensor_modes)
     config = picam2.create_still_configuration(
         main={"size": size,"format": "RGB888"}, # scale down the image, but maintain the full field of view
         raw={'size': (3280, 2464)},
@@ -30,14 +31,12 @@ def get_barycentre(frame, irow, seuil=50):
     """ renvoie le barycentre
     1. transforme en gris
     2. applique un seuil
-    3. calcule la moyenne pondérée de la ligne -10
+    3. calcule la moyenne pondérée de la ligne irow
     """
     vid_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ret, tabimage = cv2.threshold(vid_gray, seuil, 255, cv2.THRESH_BINARY)
         
     dim_y, dim_x = tabimage.shape
-    #print(dim_y)
-
     tabimage01 = tabimage.copy()
     tabimage01[tabimage==255] = 0
     tabimage01[tabimage==0] = 1
@@ -45,19 +44,16 @@ def get_barycentre(frame, irow, seuil=50):
     if sommew == 0:
         return np.nan
     barycentre = np.average(np.arange(dim_x), weights=tabimage01[irow, :])
-    
     return barycentre
 
 def droit(vitesse):
     vitesse = max(vitesse, -100)
     vitesse = min(vitesse, 100)
-    #print("droit", vitesse)
     motor_right.start(vitesse)
     
 def gauche(vitesse):
     vitesse = max(vitesse, -100)
     vitesse = min(vitesse, 100)
-    #print("gauche", -vitesse)
     motor_left.start(-vitesse)
     
 def stop():
@@ -75,6 +71,12 @@ def courbe(trajectoire, index):
     ax.axvline(x=index, color='r')
     plt.show()
 
+def temps():
+    temps = time.time()
+    duree = temps - last
+    last = temps
+    print(duree * 1000)
+
 def suivi(difference, barycentre, dernier, avant):
     if not np.isfinite(barycentre):
         print("perte de la ligne")
@@ -85,14 +87,11 @@ def suivi(difference, barycentre, dernier, avant):
         if avant > dernier:
             droite(vitesse)
     if difference == 0 :
-        #print('En avant')
         avancer(vitesse)
     if difference > 0 :
-        #print('à gauche')
         droit(vitesse)
         gauche(2 * vitesse / np.abs(difference))
     if difference < 0 :
-        #print('à droite')
         gauche(vitesse)
         droit(2 * vitesse / np.abs(difference))
 
@@ -101,15 +100,8 @@ last = 0
 lecture = False
 index = 0
 try:
-    while 1:#(video.isOpened()):
-  # lire chaque image une par une
-    #ret, frame = video.read()
+    while 1:
         frame = picam2.capture_array()
-    
-        #temps = time.time()
-        #duree = temps - last
-        #last = temps
-        #print(duree * 1000)
         dim_y, dim_x, _ = frame.shape
         centre = dim_x // 2
         barycentre = get_barycentre(frame, irow)
@@ -127,13 +119,6 @@ try:
                 break
         else:
             suivi(difference, barycentre, trajectoire[index], trajectoire[index - 1])
-        
-        #cv2.circle(frame, (barint, dim_y+irow), 20, (0, 0, 255), -1) 
-        #cv2.imshow('frame', frame)
-        #key = cv2.waitKey(20)
-        #if key == ord('q'):
-            #stop()
-            #break
 
 except KeyboardInterrupt:
     stop()
