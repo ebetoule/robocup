5c3699b05806cc117ec074e698bb6fd8006c4ffc
Mme Raspberry <eloise.betoule@free.fr>
2026-02-27 19:14:25 +0100

    Programme de calibration fonctionnel

--- calibration.py
+++ calibration.py
@@ -1,38 +1,44 @@
 import cv2
 import camera
 import numpy as np
+"""
+[[ 3.48476347e-02 -3.12056777e-03 -1.23421610e+01]
+ [ 2.97398611e-03  1.67479070e-02  3.12144966e+01]
+ [ 1.06213790e-04  5.38161659e-03  1.00000000e+00]]
+"""
+imgpoints = np.array([
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    ], dtype="float32")
+
+compteur = 0
 
 def click(action, x, y, flags, userdata):
-    ''' cliquer dans l'ordre, sans se tromper :
--avec le bouton gauche, le coin en bas à gauche
--avec le bouton droit, le coin en bas à droite
--avec la touche controle, le coin en haut à gauche
--avec la touche tab, le coin en haut à droite'''
-    if action == cv2.EVENT_LBUTTONDBLCLK:
-        x1, y1 = x, y
-    if action == cv2.EVENT_RBUTTONDBLCLK:
-        x2, y2 = x, y
-    if action == cv2.EVENT_FLAG_CTRLKEY:
-        x3, y3 = x, y
-    if action == cv2.EVENT_FLAG_ALTKEY :
-        x4, y4 = x, y
-    
+    """ cliquer dans l'ordre, sans se tromper :
+    -avec le bouton gauche, le coin en bas à gauche
+    -avec le bouton droit, le coin en bas à droite
+    -avec la touche controle, le coin en haut à gauche
+    -avec la touche tab, le coin en haut à droite"""
     objpoints = np.array([
-        [-4, 13],
-        [2, 13],
-        [-6, 28.],
-        [6,28],
-        ], dtype="float32")
-    imgpoints = np.array([
-        [x1, y1],
-        [x2, y2],
-        [x3., y3],
-        [x4, y4],
-        ], dtype="float32")
-
-#cv2.imread('tutoriel/chess/ccn11.jpg')
-M = cv2.getPerspectiveTransform(imgpoints,objpoints)
-
+    [-4, 13],
+    [2, 13],
+    [-6, 28.],
+    [6,28],
+    ], dtype="float32")
+    global imgpoints
+    global compteur
+    if action == cv2.EVENT_LBUTTONDBLCLK:
+        imgpoints[compteur,0] = x
+        imgpoints[compteur,1] = y
+        compteur = compteur + 1
+        print(f"ok pour le nummero {compteur}")
+    if compteur == 4:
+        print("ok pour le quatrième")
+        M = cv2.getPerspectiveTransform(imgpoints,objpoints)
+        print(M)
+    
 
 
 if __name__ == '__main__':
